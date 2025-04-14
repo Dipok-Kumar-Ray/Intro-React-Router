d74b06f2c9bad7679aa7b06c29645471e5159ab6
Dipok Kumar Ray <dipok7316@gmail.com>
2025-04-14 15:33:20 +0600

    Path and Element Create

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Components/Root/Root.jsx'
+import Home from './Components/Home/Home.jsx'
+import Mobiles from './Components/Mobiles/Mobiles.jsx'
+import Laptops from './Components/Laptops/Laptops.jsx'
+
+
+
+
+const router = createBrowserRouter([
+{
+  path:'/',
+  Component:Root,
+  children: [
+    {index:true, Component:Home},
+    {path:'mobiles', Component:Mobiles},
+    {path:'laptops', Component:Laptops}
+  ]
+}
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
